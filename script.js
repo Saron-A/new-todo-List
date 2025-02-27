640034887047177924641ade599f1d45f034955e
Saron <saronabebe2019@gmail.com>
2025-02-27 14:38:39 -0800

    Add local (persistent) storage and CRUD operations

--- script.js
+++ script.js
@@ -8,19 +8,72 @@ if (!todo) {
   todo = [];
 }
 
-// crud operations
+displayTasks(); // displays when the page loads
 
-addBtn.addEventListener("click", () => {
-  if (input.value !== "") {
+// crud operations
+function displayTasks() {
+  lists.innerHTML = ""; // stops repetition of the whole array every time a new item is added
+
+  todo.forEach((element) => {
+    // we need error controlling conditionals
     let div = document.createElement("div");
     div.classList.add("tasks");
 
-    let task = taskCreation(div);
-    editTask(div, task);
-    delTask(div);
+    let task = document.createElement("p");
+    task.textContent = element.todo;
+    task.setAttribute("style", "cursor: pointer; font-weight: 400;");
+    console.log(task.textContent);
 
+    task.addEventListener("dblclick", () => {
+      task.contentEditable = false;
+      task.setAttribute(
+        "style",
+        "text-decoration: line-through; font-style: italic; cursor: pointer; opacity: 0.5;"
+      );
+    });
+
+    let edit = document.createElement("button");
+    edit.textContent = "Edit";
+    edit.setAttribute("style", "margin-left: auto;");
+    edit.addEventListener("click", () => {
+      task.contentEditable = true;
+      task.focus();
+      task.setAttribute(
+        "style",
+        "background-color: #f0f0f0; cursor: text; padding: 5px; border-radius: 5px; border: 2px solid black"
+      );
+
+      task.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+          event.preventDefault();
+          task.contentEditable = false;
+
+          element.todo = task.textContent.trim();
+          localStorage.setItem("todo", JSON.stringify(todo));
+          task.classList.add("tasks");
+        }
+      });
+    });
+
+    let del = document.createElement("button");
+    del.textContent = "DEL";
+    del.addEventListener("click", () => {
+      const index = todo.indexOf(element);
+      div.remove();
+      todo.splice(index, 1);
+
+      localStorage.setItem("todo", JSON.stringify(todo));
+      displayTasks();
+    });
+
+    div.append(task, edit, del);
     lists.appendChild(div);
-    // we don't have a local storage yet
+  });
+}
+
+addBtn.addEventListener("click", () => {
+  let task = input.value;
+  if (task !== "") {
     if (!todo) {
       todo = [];
     }
@@ -28,12 +81,11 @@ addBtn.addEventListener("click", () => {
       todo: input.value,
       status: false,
     };
-
     todo.push(items);
     console.log(items);
     localStorage.setItem("todo", JSON.stringify(todo));
-
-    input.value = "";
+    displayTasks();
+    input.value = ""; // Clear input
   } else {
     let error = document.createElement("p");
     error.textContent = "Please enter a task!";
@@ -43,51 +95,3 @@ addBtn.addEventListener("click", () => {
     }, 3000);
   }
 });
-
-function readTasks(div) {
-  todo.forEach((item) => {
-    taskCreation(div).textContent = item.todo;
-  });
-}
-
-function taskCreation(div) {
-  let task = document.createElement("p");
-  task.textContent = input.value;
-  task.setAttribute("style", "cursor: pointer; opacity: 0.5;");
-
-  task.addEventListener("dblclick", () => {
-    task.contentEditable = false;
-    task.setAttribute(
-      "style",
-      "text-decoration: line-through; font-style: italic; cursor: pointer; opacity: 0.5;"
-    );
-  });
-
-  div.appendChild(task);
-  return task;
-}
-
-function editTask(div, task) {
-  let edit = document.createElement("button");
-  edit.textContent = "edit";
-  edit.setAttribute("style", "margin-left: auto;");
-  edit.addEventListener("click", () => {
-    task.contentEditable = true;
-    task.setAttribute(
-      "style",
-      "background-color: #f0f0f0; cursor: text; padding: 5px; border-radius: 5px;"
-    );
-  });
-
-  div.appendChild(edit);
-}
-
-function delTask(div) {
-  let del = document.createElement("button");
-  del.textContent = "del";
-  del.addEventListener("click", () => {
-    div.remove();
-  });
-
-  div.appendChild(del);
-}
